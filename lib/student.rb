d786e6f99ef40a54c7220b82432b0f7a44e9f5e0
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-03 04:38:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,48 +1,46 @@
 class Student
- attr_accessor :name, :grade
- attr_reader :id 
+  attr_accessor :name, :grade
+  attr_reader :id
 
-def initialize(name, grade, id = nil)
+  def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
     @id = id
-end
+  end
 
-def self.create_table
-   sql = <<-SQL
-   CREATE TABLE IF NOT EXISTS students (
-     id INTEGER PRIMARY KEY,
-     name TEXT,
-     grage INTEGER
-   )
-   SQL
+  def self.create_table
+    sql =  <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+      )
+      SQL
 
-   DB[:conn].execute(sql)
+    DB[:conn].execute(sql)
+  end
 
-end
+  def self.drop_table
+    sql =  <<-SQL
+    DROP TABLE students
+      SQL
 
-def self.drop_table
-  sql = <<-SQL
-  DROP TABLE students
-  SQL
+    DB[:conn].execute(sql)
+  end
 
-  DB[:conn].execute(sql)
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+      VALUES (?,?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT id FROM students ORDER BY id DESC LIMIT 1")[0][0]
+  end
 
-end
+  def self.create(s)
+    student = Student.new(s[:name], s[:grade])
+    student.save
+    student
+  end
 
-def save
- sql = <<-SQL
- INSERT INTO students(name, grade)
- VALUES (?,?)
- SQL
-
- DB[:conn].execute(sql, self.name, self.grade)
- @id = DB[:conn].execute("SELECT id FROM students ORDER BY id DESC LIMIT 1")[0][0]
-end
-
-def self.create(s)
-  student = Student.new(s[:name], s[:grade])
-  student.save
-  student
-end
 end
