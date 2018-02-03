8defc1cae9a0da1089ae8953ed2413c1f441869b
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-03 04:30:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,23 @@
 class Student
+ attr_accessor :name, :grade
+ attr_reader :# IDEA: 
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
 end
+
+def self.create_table
+   sql = <<-SQL 
+   CREATE TABLE IF NOT EXISTS students (
+     id INTEGER PRIMARY KEY,
+     name TEXT,
+     grage INTEGER
+   )
+   SQL 
+
+   DB[:conn].execute(sql)
+   
+end 
+
